4ca51fee9deaa849b6a6d07db3f32aa7e8030dad
MasterOz786 <hassanejaz400@gmail.com>
2025-05-04 21:45:26 +0500

    add: csv loader

---
+++ Loader.hpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#define DATASET_FILENAME "soc-sign-bitcoinotc.csv"
+
+struct Node {
+    int source; // from which user
+    int dest; // to which user
+    int weight; // rating assigned with the destination user
+    long long int timestamp; // when was the transaction performed
+};
+
+using Nodes = std::vector<Node>;
+
+class Loader {
+public:
+    Loader(bool bi = false) { }
+    static Nodes load(const char*);
+};
