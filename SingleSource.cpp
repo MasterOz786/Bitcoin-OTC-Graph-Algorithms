7f642228f995c0319464a0adf392a5b5674a58a6
MasterOz786 <hassanejaz400@gmail.com>
2025-05-07 22:05:27 +0500

    add: average degree and diameter of graph

--- SingleSource.cpp
+++ SingleSource.cpp
@@ -4,18 +4,19 @@
 #include <limits>
 #include <queue>
 
-void SingleSource::addEdge(const Node& node, bool bidirectional) {
+void SingleSource::addEdge(const Node& node) {
     adjList[node.source].emplace_back(node.dest, node.weight);
-    if (bidirectional) {
+    if (this->biDirectional) {
         adjList[node.dest].emplace_back(node.source, node.weight);
     }
 }
 
-void SingleSource::buildAdjacencyList() {
+void SingleSource::buildAdjacencyList(bool biDirectional) {
     Nodes nodes = Loader::load(BELLMANFORD_DATASET_FILENAME);
+    this->biDirectional = biDirectional;
 
     for (auto node: nodes) {
-        addEdge(node, true);
+        addEdge(node);
     }
 }
 
@@ -120,12 +121,77 @@ void SingleSource::bellmanFord(int source) {
     }
 }
 
+// Calculates the average degree
+double SingleSource::averageDegree() const {
+    int edgeCount = 0;
+    int nodeCount = adjList.size();
+
+    for (const auto& pair : adjList) {
+        edgeCount += pair.second.size();
+    }
+
+    // For undirected graph, divide edgeCount by 2 to avoid double counting
+    double avgDegree = this->biDirectional? static_cast<double>(edgeCount) / nodeCount : edgeCount; 
+    return avgDegree;
+}
+
+// Calculates the graph diameter using Dijkstra (weighted) from every node
+double SingleSource::diameter() const {
+    int maxDistance = 0;
+
+    for (const auto& [start, _] : adjList) {
+        std::unordered_map<int, int> distance;
+        for (const auto& [node, _] : adjList) {
+            distance[node] = std::numeric_limits<int>::max();
+        }
+        distance[start] = 0;
+
+        using pii = std::pair<int, int>; // (distance, node)
+        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            auto [dist, node] = pq.top();
+            pq.pop();
+
+            if (dist > distance[node]) continue;
+
+            for (const auto& [neighbor, weight] : adjList.at(node)) {
+                if (distance[node] + weight < distance[neighbor]) {
+                    distance[neighbor] = distance[node] + weight;
+                    pq.push({distance[neighbor], neighbor});
+                }
+            }
+        }
+
+        for (const auto& [node, dist] : distance) {
+            if (dist != std::numeric_limits<int>::max()) {
+                maxDistance = std::max(maxDistance, dist);
+            }
+        }
+    }
+
+    return maxDistance;
+}
+
 
 int main() {
     SingleSource ss;
-    ss.buildAdjacencyList();
-    ss.dijkstra(6);
-    ss.bellmanFord(6);
+
+    ss.buildAdjacencyList(true);
+
+    int sourceNode;
+
+    std::cout << "Source Node for Dijkstra: ";
+    std::cin >> sourceNode;
+    ss.dijkstra(sourceNode);
+
+    std::cout << "Source Node for Bellman Ford: ";
+    std::cin >> sourceNode;
+    ss.bellmanFord(sourceNode);
+    
+    std::cout << "Average Degree => " << ss.averageDegree() << '\n';
+    std::cout << "Diameter of the Graph => " << ss.diameter() << '\n';
 
     return 0;
 }
--- SingleSource.hpp
+++ SingleSource.hpp
@@ -7,12 +7,16 @@ using AdjacencyList = std::unordered_map<int, std::vector<EdgeWeight>>;
 
 class SingleSource {
     AdjacencyList adjList;
+    bool biDirectional = false;
 
 public:
-    void addEdge(const Node&, bool);
+    void addEdge(const Node&);
     void printAdjList() const;
-    void buildAdjacencyList();
+    void buildAdjacencyList(bool);
     const AdjacencyList& getAdjacencyList() const;
     void dijkstra(int);
     void bellmanFord(int);
+
+    double averageDegree() const;
+    double diameter() const;
 };
